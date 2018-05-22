098e68b831d695c9833c3176d4c15c0ecf11db74
edwinrodriguez <mredwinrodriguez@gmail.com>
2018-05-22 11:22:40 -0400

    recipe list structure

--- my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'this is simply a test', 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg'),
+    new Recipe('A Test Recipe', 'this is simply a test', 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg')
+
+  ];
 
   constructor() { }
 
