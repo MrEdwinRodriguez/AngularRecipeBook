1f48eecafff00559959c6907364b2774bae1ecdb
edwinrodriguez <mredwinrodriguez@gmail.com>
2018-05-31 20:56:12 -0400

    added recipe details

--- my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'this is simply a test', 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg'),
-    new Recipe('A Test Recipe', 'this is simply a test', 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg')
+    new Recipe('A Different Recipe', 'this is simply a test', 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg')
 
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
